327ea50f76579f9a3522e4f7d894c86d1433da88
Autumn Pan <pan662607@gmail.com>
2025-08-06 21:09:01 -0400

    add sleep function

--- src/kernel/sched.c
+++ src/kernel/sched.c
@@ -1,5 +1,6 @@
 #include "kernel/sched.h"
 #include "util/list.h"
+#include "kernel/clock.h"
 
 void add_priority(uint32_t priority) {
     if (priority < 32) {
@@ -27,13 +28,12 @@ TBC* scheduler_next_task()
     return ready_queue[get_highest_priority()]->head;
 }
 
-void set_state_queue(TBC* tbc, enum TASK_STATE queue)
-{
-    uint8_t index = find_node_index(tbc->state, tbc);
-    // Remove TBC from its current queue
-    list_pop(tbc->state, index);
+// Change the state of a task
+// This switches its queue, updates the priority bitset, and changes its internal state
+// This does not include handling for sleep or other utilities
 
-    // Update ready bitset if needed
+void update_ready_bitset(TBC* tbc)
+{
     if(tbc->state == READY)
     {
         if(ready_queue[tbc->priority]->size == 0)
@@ -41,6 +41,16 @@ void set_state_queue(TBC* tbc, enum TASK_STATE queue)
             remove_priority(tbc->priority);
         }
     }
+}
+void set_state_queue(TBC* tbc, enum TASK_STATE queue)
+{
+    uint8_t index = find_node_index(tbc->state, tbc);
+    // Remove TBC from its current queue
+    list_pop(tbc->state, index);
+
+    // Update ready bitset if needed
+    update_ready_bitset(tbc);
+
     tbc->state = queue;
 
     if(queue == READY)
@@ -56,3 +66,20 @@ void set_state_queue(TBC* tbc, enum TASK_STATE queue)
     list_append(queue, tbc);
 }
 
+void sleep(uint32_t time)
+{
+    // Set up sleep controller
+    running->sleep_controller->init_time = get_ticks();
+    running->sleep_controller->sleep_time = time;
+    running->sleep_controller->remaining_time = time;
+
+    // Mark the running ask as asleep
+    set_state_queue(running, SLEEPING);
+
+    // Provisional solution; will eventually use linked lists to determine next task in advance
+    TBC* next_task = scheduler_next_task();
+    switch_context(running, next_task);
+    // If necessary, update ready bitset
+    update_ready_bitset(next_task);
+    running = next_task;
+}
