1568c9012bdfaa76c6d6b090d433c12c85d3b7b9
Autumn Pan <pan662607@gmail.com>
2025-08-10 20:17:09 -0400

    hotfixes

---
+++ build/CMakeFiles/albatross.elf.dir/main.c.obj.d
@@ -0,0 +1,11 @@
+CMakeFiles/albatross.elf.dir/main.c.obj: \
+ /home/autumn/Documents/os/albatross/main.c \
+ /home/autumn/Documents/os/albatross/include/kernel/os.h \
+ /home/autumn/Documents/os/albatross/include/kernel/clock.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.0/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.0/include/stdint-gcc.h \
+ /home/autumn/Documents/os/albatross/include/kernel/sched.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.0/include/stddef.h \
+ /home/autumn/Documents/os/albatross/include/kernel/../util/list.h \
+ /home/autumn/Documents/os/albatross/include/kernel/task.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.0/include/stdbool.h
---
+++ build/CMakeFiles/albatross.elf.dir/src/kernel/sched.c.obj.d
@@ -0,0 +1,11 @@
+CMakeFiles/albatross.elf.dir/src/kernel/sched.c.obj: \
+ /home/autumn/Documents/os/albatross/src/kernel/sched.c \
+ /home/autumn/Documents/os/albatross/include/kernel/sched.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.0/include/stddef.h \
+ /home/autumn/Documents/os/albatross/include/kernel/../util/list.h \
+ /home/autumn/Documents/os/albatross/include/kernel/task.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.0/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.0/include/stdint-gcc.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.0/include/stdbool.h \
+ /home/autumn/Documents/os/albatross/include/util/list.h \
+ /home/autumn/Documents/os/albatross/include/kernel/clock.h
