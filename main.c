74cadf3f06f895d86b89ef8c549c5222bb2391d3
Autumn Pan <pan662607@gmail.com>
2025-08-14 13:46:30 -0400

    add test file

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include "kernel/os.h"
 
+uint8_t i = 0;
+uint8_t j = 0;
+
+
+void task_func1()
+{
+    while(1)
+    {
+        i++;
+        yield();
+    }
+}
+
+void task_func2()
+{
+    while (1)
+    {
+        j++;
+        yield();
+    }
+}
+
 int main()
 {
+    TBC* task1 = init_tbc(task_func1, 1);
+    add_task(task1);
+    TBC* task2 = init_tbc(task_func1, 1);
+    add_task(task2);
     return 0;
 }
